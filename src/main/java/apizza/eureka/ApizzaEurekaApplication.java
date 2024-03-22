c27647ffd2fd27a86e78763c9ad66f86cf1a5b41
Maksim Yaskov <m.yaskov@astondevs.ru>
2024-03-22 22:11:15 +0300

    Initial commit

---
+++ src/main/java/apizza/eureka/ApizzaEurekaApplication.java
@@ -0,0 +1,15 @@
+package apizza.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ApizzaEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApizzaEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/apizza/eureka/ApizzaEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package apizza.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApizzaEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
